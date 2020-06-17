3fe4152723dbc77e2af4265b53f09b78256ad08d
wangjun <05crazy16@sina.com>
2020-06-17 16:37:01 +0800

    添加原生app体验

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,8 @@ module.exports = {
     title: '米斯特王的博客',
     head: [ // 注入到当前页面的 HTML <head> 中的标签
       ['link', { rel: 'icon', href: '/avatar.png' }], // 增加一个自定义的 favicon(网页标签的图标)
+      ['link', { rel: 'manifest', href: '/avatar.png' }],
+      ['link', { rel: 'apple-touch-icon', href: '/avatar.png' }]
     ],
     themeConfig: {
       logo: '/avatar.png',  // 左上角logo
@@ -11,5 +13,6 @@ module.exports = {
         {text: '简书主页', link: 'https://www.jianshu.com/u/c455567c7f50'}      
       ],
       sidebar: 'auto', // 侧边栏配置
-    }
+    },
+    serviceWorker: true // 是否开启 PWA
 };
