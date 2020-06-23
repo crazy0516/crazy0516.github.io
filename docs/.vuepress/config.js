2da3b6b25430941143467eac512485348bdb7f50
wangjun <05crazy16@sina.com>
2020-06-23 17:47:51 +0800

    修改技术文档路径

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,7 +9,7 @@ module.exports = {
       logo: '/avatar.png',  // 左上角logo
       nav:[ // 导航栏配置
         {text: '首页', link: '/' },
-        {text: '技术文档', link: '/tech/interview/' },
+        {text: '技术文档', link: '/mistewang/' },
         {text: '简书主页', link: 'https://www.jianshu.com/u/c455567c7f50'}      
       ],
       sidebar: 'auto', // 侧边栏配置
--- docs/README.md
+++ docs/README.md
@@ -4,7 +4,7 @@ heroImage: /avatar.png
 heroText: 我的主页
 tagline: My homepage
 actionText: 技术文档 →
-actionLink: /tech/interview/
+actionLink: /mistewang/
 features:
 - title: 简洁至上
   details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
