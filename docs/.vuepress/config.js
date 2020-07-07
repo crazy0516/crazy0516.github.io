a5e82466bf8b2edba4da212ffae5d2506f6dd6e6
wangjun <05crazy16@sina.com>
2020-07-07 15:20:10 +0800

    修改

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,7 @@
 module.exports = {
     title: '米斯特王的博客',
     head: [ // 注入到当前页面的 HTML <head> 中的标签
-      ['link', { rel: 'icon', href: '/avatar.png' }], // 增加一个自定义的 favicon(网页标签的图标)
-      ['link', { rel: 'manifest', href: '/avatar.png' }],
-      ['link', { rel: 'apple-touch-icon', href: '/avatar.png' }]
+      ['link', { rel: 'icon', href: '/avatar.png' }] // 增加一个自定义的 favicon(网页标签的图标)
     ],
     themeConfig: {
       logo: '/avatar.png',  // 左上角logo
@@ -13,6 +11,5 @@ module.exports = {
         {text: '简书主页', link: 'https://www.jianshu.com/u/c455567c7f50'}      
       ],
       sidebar: 'auto', // 侧边栏配置
-    },
-    serviceWorker: true // 是否开启 PWA
+    }
 };
